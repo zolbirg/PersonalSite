6d4f8985adb358ca1bb8a331dd29b2d024ecdeef
Kanin Roman <kaninroman@mail.ru>
2025-03-18 21:08:58 +0300

    Settings eslint and prettier

--- Site v 1/src/App.jsx
+++ Site v 1/src/App.jsx
@@ -7,32 +7,32 @@
 // import Contact from "./components/contact/Contact.jsx";
 // import ColorPalete from './assets/colororPlate/ColorPlate.jsx'
 
-import { Routes, Route } from "react-router-dom";
-import Layout from "./assets/layout/Layout.jsx";
-import HomePage from "./pages/homePages/homePage.jsx";
-import ProjectPage from "./pages/portfolio/ProjectPage.jsx";
-import ColorSwitch from "./pages/portfolio/project/web/colorSwitch/СolorSwitch.jsx";
-import NotFoundPage from "./pages/NotFoundPage.jsx";
+import { Routes, Route } from 'react-router-dom';
+import Layout from './assets/layout/Layout.jsx';
+import HomePage from './pages/homePages/homePage.jsx';
+import ProjectPage from './pages/portfolio/ProjectPage.jsx';
+import ColorSwitch from './pages/portfolio/project/web/colorSwitch/СolorSwitch.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
 
 const App = () => {
-  return (
-    <>
-      {/* <NavBar />  */}
+    return (
+        <>
+            {/* <NavBar />  */}
 
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="Project" element={<ProjectPage />} />
-          <Route path="ColorSwitch" element={<ColorSwitch />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="Project" element={<ProjectPage />} />
+                    <Route path="ColorSwitch" element={<ColorSwitch />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Route>
+            </Routes>
 
-      {/*      
+            {/*      
         <TestComp />
         <ColorPalete /> */}
-    </>
-  );
+        </>
+    );
 };
 
 export default App;
